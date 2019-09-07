ca41be48ca6bd4603ab4db1b65aa27c1a4742fe9
Nnamdi Okeke <nahorr@gmail.com>
2019-09-07 12:15:40 -0600

    school set up views

--- resources/views/admin/superadmin/schoolsetup/terms/addTermModal.blade.php
+++ resources/views/admin/superadmin/schoolsetup/terms/addTermModal.blade.php
@@ -1,12 +1,31 @@
 <!-- The Modal -->
 <div class="modal fade" id="addTermModal-{{$schoolyear->id}}">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <!-- Modal Header -->
             <div class="modal-header" style="background-color: #7FB3D5; color: #FFF">
               <h4 class="modal-title"><strong>Add Term Form</strong></h4>
               <hr>
-              <h5 class="modal-title">School year: {{$schoolyear->school_year}} </h5>
+              <h5 class="modal-title">
+                <ul class="list-unstyled spaced2">
+                    <li>
+                        <ul class="list-inline">
+                            <li>
+                                <i class="fa fa-circle"></i> School year: {{$schoolyear->school_year}}
+                            </li>
+                            <li>
+                                <i class="fa fa-circle"></i> Start date: {{$schoolyear->start_date->toformatteddateString()}}
+                            </li>
+                            <li>
+                                <i class="fa fa-circle"></i> End Date: {{$schoolyear->end_date->toFormatteddateString()}}
+                            </li>
+                            <li>
+                                <i class="fa fa-circle"></i> Show Until: {{$schoolyear->end_date->toFormatteddateString()}}
+                            </li>
+                        </ul>
+                    </li>
+                </ul>      
+            </h5>
               <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>            
             <!-- Modal body -->
