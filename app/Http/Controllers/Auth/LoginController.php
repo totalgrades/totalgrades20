4aeab42bc0d4d02466ce4e765654b18547931691
nnamdi <nnamdi@totalgrades.totalgrades.dev>
2023-09-30 21:48:01 -0600

    re-install after a while

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -57,6 +57,6 @@ class LoginController extends Controller
     {
         //return $request->only($this->username(), 'password');
 
-        return ['email'=>$request{$this->username()}, 'password'=>$request->password, 'status'=>'1'];
+        return ['email'=>$request[$this->username()], 'password'=>$request->password, 'status'=>'1'];
     }
 }
