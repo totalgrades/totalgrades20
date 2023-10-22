417cff942255d82ab88096b7c801e56b3888ccf3
Totalgrades <totalgrades@gmail.com>
2023-10-22 06:58:25 -0600

    report cards

--- app/Grades/LetterGrade.php
+++ app/Grades/LetterGrade.php
@@ -24,13 +24,13 @@ class LetterGrade
             return "B-";
         }elseif ($total_grade<= 86 && $total_grade >=84){
             return "B";
-        }elseif ($total_grade<= 89 && $total_grade >=87){
+        }elseif ($total_grade < 90 && $total_grade >=87){
             return "B+";
-        }elseif ($total_grade<= 93 && $total_grade >=90){
+        }elseif ($total_grade < 94 && $total_grade >=90){
             return "A-";
-        }elseif ($total_grade<= 96 && $total_grade >=94){
+        }elseif ($total_grade < 97 && $total_grade >=94){
             return "A";
-        }elseif ($total_grade>= 97){
+        }elseif ($total_grade >= 97){
             return "A+";
         }
 
