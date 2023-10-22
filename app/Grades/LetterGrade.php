4f59eac65af36340e26524ce176f8e1418085686
Totalgrades <totalgrades@gmail.com>
2023-10-22 07:00:33 -0600

    report cards

--- app/Grades/LetterGrade.php
+++ app/Grades/LetterGrade.php
@@ -10,19 +10,19 @@ class LetterGrade
 
         if ($total_grade < 65){
             return "F";
-        }elseif ($total_grade<= 66 && $total_grade >=65){
+        }elseif ($total_grade < 67 && $total_grade >=65){
             return "D";
-        }elseif ($total_grade<= 69 && $total_grade >=67){
+        }elseif ($total_grade < 70 && $total_grade >=67){
             return "D+";
-        }elseif ($total_grade<= 73 && $total_grade >=70){
+        }elseif ($total_grade < 74 && $total_grade >=70){
             return "C-";
-        }elseif ($total_grade<= 76 && $total_grade >=74){
+        }elseif ($total_grade < 77 && $total_grade >=74){
             return "C";
-        }elseif ($total_grade<= 79 && $total_grade >=77){
+        }elseif ($total_grade < 80 && $total_grade >=77){
             return "C+";
-        }elseif ($total_grade<= 83 && $total_grade >=80){
+        }elseif ($total_grade < 84 && $total_grade >=80){
             return "B-";
-        }elseif ($total_grade<= 86 && $total_grade >=84){
+        }elseif ($total_grade < 87 && $total_grade >=84){
             return "B";
         }elseif ($total_grade < 90 && $total_grade >=87){
             return "B+";
